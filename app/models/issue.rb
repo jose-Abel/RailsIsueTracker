7cec8eabae61e0ce2e0b5cc059378a022cbfaa51
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-10-29 08:21:27 -0400

    Create the pages_controller, home.html.erb view, about.html.erb view and the table for the Issues in the db

---
+++ app/models/issue.rb
@@ -0,0 +1,5 @@
+class Issue < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
+
+  validates :description, presence: true, length: {minimum: 10, maximum: 10000}
+end
