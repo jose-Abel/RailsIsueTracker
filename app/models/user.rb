224478da22f3f305072e1dea7720d085ceb5282e
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-19 20:11:26 -0400

    Add user_id to the projects column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
+  enum role: [:demo, :admin, :teamlead, :developer, :designer, :qualityagent]
+  
+  has_many :projects, dependent: :destroy
+  
   validates :username, presence: true, uniqueness:  { case_sensitive: false}, length: { minimum: 3, maximum: 25 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
