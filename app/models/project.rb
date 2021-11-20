8c1fc8b83c04fa4c4a7cf14b29fe5cca8c5718b5
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-19 20:18:01 -0400

    Add the belongs_to user for the projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   validates :title, presence: true, length: {minimum: 6, maximum: 100}
 
   validates :description, presence: true, length: {minimum: 10, maximum: 10000}
+
+  belongs_to :user
 end
